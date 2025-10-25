a388c3887cf24c92adb600fb9cda1da5b75a01dd
Khushi Choudhary <157037572+Kkkhush@users.noreply.github.com>
2025-10-25 19:15:39 +0000

    Add file explaining conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score ="33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+//special type --- NaN(Not a number)
+
+
+// agar score null hota toh type 0 hota
+// agar scopre true hot atoh va;uye 1 display krta
+
+// string ko number me convert krte toh NaN aa jaata
+
+//let isLoggedIn=1  // converted into true
+
+//let booleanIsLoggegIn= Boolean(isLoggedIn);
+
+
+let isLoggedIn=""  // converted into false
+
+let BooleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
