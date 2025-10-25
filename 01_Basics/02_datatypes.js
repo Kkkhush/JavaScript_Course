f51c566f2f421294c4085943cc351d2b77e45471
Khushi Choudhary <157037572+Kkkhush@users.noreply.github.com>
2025-10-25 18:55:47 +0000

    Add file explaining basic data types

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,44 @@
+"use strict";   // treat all new js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+// alert dialog should be used for messages which do not require any response on the part of the user, other than the acknowledgement of the message.
+
+// avoid semicolon mostly
+//  code readability should be high
+
+// ECMA
+// Ecma International (formally European Computer Manufacturers Association) is a non-profit organization that develops standards in computer hardware, communications, and programming languages.
+
+// there aRE MANY DATATYPES IN JAVASCript
+
+let name= "Khushi" // string
+let age= 3 // number
+
+let isLoggedIn= false // boolean 
+let state; //undefined
+
+
+// PRIMITIVE DATATYPES
+
+// number => 2 to power 53
+
+// bigint 
+// string => " "
+// boolean=> true or false
+
+//null => standalone value  // empty value //defined hai pr va;ue assign nhi kiya
+
+//undefined => abhi value assign nhi hui hai
+
+//symbol=> react components me use aata hai // uniqueness ke liye use hota hai
+
+// object
+
+// IMPORTANT
+
+
+console.log(typeof "hitesh"); 
+console.log(typeof null); // type object aaega
+
+console.log(typeof undefined);
